06b77d5d85ac82cd43acc8ce7cf5bbcc6e4f8afa
Igor Lucas <igorlucas.silva@bol.com.br>
2019-08-16 21:58:39 -0300

    endpoint pedido/id

--- src/main/java/com/cursomc/main/services/ClienteService.java
+++ src/main/java/com/cursomc/main/services/ClienteService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.cursomc.main.domain.Categoria;
 import com.cursomc.main.domain.Cliente;
 import com.cursomc.main.repositories.ClienteRepository;
 import com.cursomc.main.services.exceptios.ObjectNotFoundException;
@@ -19,6 +18,6 @@ public class ClienteService {
 	public Cliente buscar(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 
-		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado, id:"+id+" Tipo: "+Categoria.class.getName()));
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado, id:"+id+" Tipo: "+Cliente.class.getName()));
 	}
 }
---
+++ src/main/java/com/cursomc/main/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.cursomc.main.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursomc.main.domain.Pedido;
+import com.cursomc.main.repositories.PedidoRepository;
+import com.cursomc.main.services.exceptios.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado, id:"+id+" Tipo: "+Pedido.class.getName()));
+	}
+}
