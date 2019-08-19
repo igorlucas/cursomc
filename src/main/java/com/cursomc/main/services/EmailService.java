80df6cf7d0725490862c839eea47debbc9ed6951
Igor Lucas <igorlucas.silva@bol.com.br>
2019-08-19 19:58:11 -0300

    Email HTML

--- src/main/java/com/cursomc/main/services/EmailService.java
+++ src/main/java/com/cursomc/main/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.cursomc.main.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.cursomc.main.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido pedido);
 	
 	void sendEmail(SimpleMailMessage email);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
